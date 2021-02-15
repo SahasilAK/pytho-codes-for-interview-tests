ab49baaf0ecbdd5c054e1642f421513ea5d8bd82
Sahasil AK <76594303+SahasilAK@users.noreply.github.com>
2021-02-15 12:35:33 +0530

    Update Fill_The_Blanks.py

--- Fill_The_Blanks.py
+++ Fill_The_Blanks.py
@@ -3,12 +3,13 @@
 array1 = [1,None,2,3,None,None,5,None]
 
 def solution(array):
-    valid = 0            
+    valid = 0
     res = []
-    for i in nums:
+    for i in array:
         if i is not None:
             res.append(i)
             valid = i
         else:
             res.append(valid)
     return res
+print(solution(array1))
