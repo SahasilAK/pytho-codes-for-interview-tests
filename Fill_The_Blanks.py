4c12471dccd24e100de12d0f5538d6f8cf80b5a3
SahasilAK <aksahasill@gmail.com>
2021-03-02 12:47:00 +0530

    Update Fill_The_Blanks.py

--- Fill_The_Blanks.py
+++ Fill_The_Blanks.py
@@ -1,6 +1,6 @@
 # Given an array containing None values fill in the None values with most recent
 # non None value in the array
-array1 = [1,None,2,3,None,None,5,None]
+array = [1,None,2,3,None,None,5,None]
 
 def solution(array):
     valid = 0
